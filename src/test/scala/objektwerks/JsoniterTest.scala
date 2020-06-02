f828de88d24c92321f876dc0d36eaaab49d295ae
objektwerks <objektwerks@runbox.com>
2020-06-02 12:05:08 -0400

    enhanced jsoniter test

--- src/test/scala/objektwerks/JsoniterTest.scala
+++ src/test/scala/objektwerks/JsoniterTest.scala
@@ -4,6 +4,7 @@ import org.scalatest.{FunSuite, Matchers}
 import org.slf4j.LoggerFactory
 
 sealed trait Person extends Product with Serializable {
+  def id: String
   def name: String
 }
 
@@ -11,7 +12,7 @@ object Person {
   import com.github.plokhotnyuk.jsoniter_scala.core._
   import com.github.plokhotnyuk.jsoniter_scala.macros._
 
-  implicit val codec: JsonValueCodec[Person] = JsonCodecMaker.make[Person](CodecMakerConfig)
+  implicit val personCode: JsonValueCodec[Person] = JsonCodecMaker.make[Person](CodecMakerConfig)
 }
 
 case class Employee(id: String, name: String) extends Person
@@ -20,18 +21,18 @@ object Employee {
   import com.github.plokhotnyuk.jsoniter_scala.core._
   import com.github.plokhotnyuk.jsoniter_scala.macros._
 
-  implicit val codec: JsonValueCodec[Employee] = JsonCodecMaker.make[Employee](CodecMakerConfig)
+  implicit val employeeCodec: JsonValueCodec[Employee] = JsonCodecMaker.make[Employee](CodecMakerConfig)
 }
 
 class JsoniterTest extends FunSuite with Matchers {
   val logger = LoggerFactory.getLogger(getClass)
 
   test("jsoniter") {
-    import Employee._
     import com.github.plokhotnyuk.jsoniter_scala.core._
 
+    import Employee._
     val employee = Employee("goldfinger", "auric goldfinger")
-    employee.isInstanceOf[Employee] shouldBe true
+    employee.isInstanceOf[Employee]
     val employeeAsJson = writeToArray(employee)
 
     logger.info(s"jsoniter employee: $employee")
@@ -39,7 +40,8 @@ class JsoniterTest extends FunSuite with Matchers {
     
     employee shouldBe readFromArray(employeeAsJson)
 
-    val person: Person = Employee("bond", "james bond")
+    import Person._
+    val person = employee.asInstanceOf[Person]
     person.isInstanceOf[Person] shouldBe true
     val personAsJson = writeToArray(person)
 
