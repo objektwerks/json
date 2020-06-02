a5967e19a0e781adfdbffdbaa123470451d79cde
objektwerks <objektwerks@runbox.com>
2020-06-02 11:39:50 -0400

    enhanced jsoniter test

--- src/test/scala/objektwerks/JsoniterTest.scala
+++ src/test/scala/objektwerks/JsoniterTest.scala
@@ -3,7 +3,18 @@ package objektwerks
 import org.scalatest.{FunSuite, Matchers}
 import org.slf4j.LoggerFactory
 
-case class Employee(id: String, name: String)
+sealed trait Person extends Product with Serializable {
+  def name: String
+}
+
+object Person {
+  import com.github.plokhotnyuk.jsoniter_scala.core._
+  import com.github.plokhotnyuk.jsoniter_scala.macros._
+
+  implicit val codec: JsonValueCodec[Person] = JsonCodecMaker.make[Person](CodecMakerConfig)
+}
+
+case class Employee(id: String, name: String) extends Person
 
 object Employee {
   import com.github.plokhotnyuk.jsoniter_scala.core._
@@ -20,11 +31,21 @@ class JsoniterTest extends FunSuite with Matchers {
     import com.github.plokhotnyuk.jsoniter_scala.core._
 
     val employee = Employee("goldfinger", "auric goldfinger")
+    employee.isInstanceOf[Employee] shouldBe true
     val employeeAsJson = writeToArray(employee)
 
     logger.info(s"jsoniter employee: $employee")
     logger.info(s"jsoniter employee as json: ${employeeAsJson.mkString}")
     
     employee shouldBe readFromArray(employeeAsJson)
+
+    val person: Person = Employee("bond", "james bond")
+    person.isInstanceOf[Person] shouldBe true
+    val personAsJson = writeToArray(person)
+
+    logger.info(s"jsoniter person: $person")
+    logger.info(s"jsoniter person as json: ${personAsJson.mkString}")
+    
+    person shouldBe readFromArray(personAsJson)
   }
 }
