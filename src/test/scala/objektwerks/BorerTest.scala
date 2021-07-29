245358226ecb6d712dcefef4bdf1e53c9c306431
objektwerks <objektwerks@runbox.com>
2021-07-29 07:49:07 -0400

    added borer test template

---
+++ src/test/scala/objektwerks/BorerTest.scala
@@ -0,0 +1,13 @@
+package objektwerks
+
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
+import org.slf4j.LoggerFactory
+
+class BorerTest extends AnyFunSuite with Matchers {
+  val logger = LoggerFactory.getLogger(getClass)
+
+  test("borer") {
+
+  }
+}
