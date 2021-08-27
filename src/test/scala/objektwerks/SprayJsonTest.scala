878e395e05df078e1ed604a4f7e225d5317d0415
objektwerks <objektwerks@runbox.com>
2021-08-27 08:55:38 -0400

    enhanced spray json test

--- src/test/scala/objektwerks/SprayJsonTest.scala
+++ src/test/scala/objektwerks/SprayJsonTest.scala
@@ -4,7 +4,7 @@ import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 import org.slf4j.LoggerFactory
 
-import spray.json.DefaultJsonProtocol
+import spray.json.{DefaultJsonProtocol, JsValue, enrichAny}
 
 sealed trait Color extends Product with Serializable {
   val name: String
@@ -15,6 +15,12 @@ final case class Navy(name: String, code: String) extends Color
 
 object ColorProtocol extends DefaultJsonProtocol {
   implicit val navyFormat = jsonFormat2(Navy)
+
+  def write(color: Color): JsValue = color match {
+    case navy: Navy => navy.toJson
+  }
+
+  def read(json: JsValue): Color = json.convertTo[Navy]
 }
 
 class SprayJsonTest extends AnyFunSuite with Matchers {
@@ -31,5 +37,14 @@ class SprayJsonTest extends AnyFunSuite with Matchers {
     logger.info(s"spray json navy as json: $navyJson")
 
     navy shouldBe navyJson.convertTo[Navy]
+
+    val color = navy.asInstanceOf[Color]
+    color.isInstanceOf[Color] shouldBe true
+    val colorJson = ColorProtocol.write(color)
+
+    logger.info(s"spray json color: $color")
+    logger.info(s"spray json color as json: $colorJson")
+
+    color shouldBe ColorProtocol.read(colorJson)
   }
 }
