256c093adec23b0af5e17c7b1a043ca13117c0ad
objektwerks <objektwerks@runbox.com>
2021-09-05 10:10:26 -0400

    replaced logging with println

--- src/test/scala/objektwerks/ZioJsonTest.scala
+++ src/test/scala/objektwerks/ZioJsonTest.scala
@@ -2,7 +2,6 @@ package objektwerks
 
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
-import org.slf4j.LoggerFactory
 
 import zio.json._
 
@@ -17,16 +16,14 @@ object Car {
 }
 
 class ZioJsonTest extends AnyFunSuite with Matchers {
-  val logger = LoggerFactory.getLogger(getClass)
-
   test("zio") {
     import Car._
 
     val porsche = Porsche("911", 1996)
     val porscheJson = porsche.toJson
 
-    logger.info(s"zio make: $porsche")
-    logger.info(s"zio make as json: $porscheJson")
+    println(s"zio make: $porsche")
+    println(s"zio make as json: $porscheJson")
 
     porsche shouldBe porscheJson.fromJson[Porsche].getOrElse( Porsche("", 0) )
 
@@ -34,8 +31,8 @@ class ZioJsonTest extends AnyFunSuite with Matchers {
     car.isInstanceOf[Car] shouldBe true
     val carJson = car.toJson
 
-    logger.info(s"zio car: $car")
-    logger.info(s"zio car as json: $carJson")
+    println(s"zio car: $car")
+    println(s"zio car as json: $carJson")
 
     car shouldBe carJson.fromJson[Car].getOrElse( Porsche("", 0) )
   }
