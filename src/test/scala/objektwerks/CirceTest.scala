18e9c9faff23a3da50aecb1d5f469c1fff3c90c9
objektwerks <objektwerks@runbox.com>
2021-09-05 10:07:27 -0400

    replaced logging with println

--- src/test/scala/objektwerks/CirceTest.scala
+++ src/test/scala/objektwerks/CirceTest.scala
@@ -2,14 +2,11 @@ package objektwerks
 
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
-import org.slf4j.LoggerFactory
 
 sealed trait Work extends Product with Serializable
 final case class Task(worker: String, work: String) extends Work
 
 class CirceTest extends AnyFunSuite with Matchers {
-  val logger = LoggerFactory.getLogger(getClass)
-
   test("circe") {
     import io.circe.generic.auto._
     import io.circe.syntax._
@@ -17,8 +14,8 @@ class CirceTest extends AnyFunSuite with Matchers {
     val task = Task("auric goldfinger", "stack gold")
     val taskJson = task.asJson
 
-    logger.info(s"circe task: $task")
-    logger.info(s"circe task as json: $taskJson")
+    println(s"circe task: $task")
+    println(s"circe task as json: $taskJson")
 
     task shouldBe taskJson.as[Task].toOption.get
 
@@ -26,8 +23,8 @@ class CirceTest extends AnyFunSuite with Matchers {
     work.isInstanceOf[Work] shouldBe true
     val workJson = work.asJson
 
-    logger.info(s"circe work: $work")
-    logger.info(s"circe work as json: $workJson")
+    println(s"circe work: $work")
+    println(s"circe work as json: $workJson")
 
     work shouldBe workJson.as[Work].toOption.get
   }
