f5bc7d920552fc653a765eeafb2f6f1a5a9bbe63
objektwerks <objektwerks@runbox.com>
2021-09-05 10:06:51 -0400

    replaced logging with println

--- src/test/scala/objektwerks/BorerTest.scala
+++ src/test/scala/objektwerks/BorerTest.scala
@@ -5,7 +5,6 @@ import io.bullet.borer.derivation.ArrayBasedCodecs._
 
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
-import org.slf4j.LoggerFactory
 
 sealed trait Canine extends Product with Serializable
 final case class Dog(breed: String, name: String) extends Canine
@@ -16,16 +15,14 @@ object Canine {
 }
 
 class BorerTest extends AnyFunSuite with Matchers {
-  val logger = LoggerFactory.getLogger(getClass)
-
   test("borer") {
     import Canine._
 
     val dog = Dog("bulldog", "spike")
     val dogJson = Json.encode(dog).toUtf8String
 
-    logger.info(s"borer dog: $dog")
-    logger.info(s"borer dog as json: $dogJson")
+    println(s"borer dog: $dog")
+    println(s"borer dog as json: $dogJson")
 
     dog shouldBe Json.decode(dogJson.getBytes("UTF8")).to[Dog].value
 
@@ -33,8 +30,8 @@ class BorerTest extends AnyFunSuite with Matchers {
     canine.isInstanceOf[Canine] shouldBe true
     val canineJson = Json.encode(canine).toUtf8String
 
-    logger.info(s"borer canine: $canine")
-    logger.info(s"borer canine as json: $canineJson")
+    println(s"borer canine: $canine")
+    println(s"borer canine as json: $canineJson")
 
     canine shouldBe Json.decode(canineJson.getBytes("UTF8")).to[Canine].value
   }
