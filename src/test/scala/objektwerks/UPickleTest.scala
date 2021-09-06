2010033583afb1ebbd3a2c7b7ededdaf230e889c
objektwerks <objektwerks@runbox.com>
2021-09-06 09:01:34 -0400

    refactored upickle test

--- src/test/scala/objektwerks/UPickleTest.scala
+++ src/test/scala/objektwerks/UPickleTest.scala
@@ -3,55 +3,26 @@ package objektwerks
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
-sealed trait Message extends Product with Serializable
-object Message {
-  import upickle.default._
-
-  implicit val messageRW: ReadWriter[Message] = ReadWriter.merge( macroRW[Entity] )
-}
-
-sealed trait Entity extends Message
-object Entity {
-  import upickle.default._
-
-  implicit val entityRW: ReadWriter[Entity] = ReadWriter.merge( macroRW[Company] )
-}
-
-final case class Company(name: String, address: String) extends Entity
-object Company {
-  import upickle.default._
-
-  implicit val readWriter: ReadWriter[Company] = macroRW
-}
-
 class UPickleTest extends AnyFunSuite with Matchers {
   test("upickle") {
+    import UPickleCodecs._
     import upickle.default._
 
-    val company = Company("objektwerks", "1 Beach Rd., Boca Grande, FL 12345")
-    val companyJson = write(company)
+    val employee = Employee(1, "James Bond")
+    val employeeJson = write(employee)
 
-    println(s"upickle company: $company")
-    println(s"upickle company as json: $companyJson")
+    println(s"upickle employee: $employee")
+    println(s"upickle employee json: $employeeJson")
 
-    company shouldBe read[Company](companyJson)
+    employee shouldBe read[Employee](employeeJson)
 
-    val entity = company.asInstanceOf[Entity]
-    entity.isInstanceOf[Entity] shouldBe true
-    val entityJson = write(entity)
+    val person = employee.asInstanceOf[Person]
+    person.isInstanceOf[Person] shouldBe true
+    val entityJson = write(person)
 
-    println(s"upickle entity: $entity")
-    println(s"upickle entity as json: $entityJson")
+    println(s"upickle person: $person")
+    println(s"upickle person json: $entityJson")
 
-    entity shouldBe read[Entity](entityJson)
-
-    val message = company.asInstanceOf[Message]
-    message.isInstanceOf[Message] shouldBe true
-    val messageJson = write(message)
-
-    println(s"upickle message: $message")
-    println(s"upickle message as json: $messageJson")
-
-    message shouldBe read[Message](messageJson)
+    person shouldBe read[Person](entityJson)
   }
 }
