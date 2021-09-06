cff0c74c0eda45bcb529c6b54bfea544ae006c1e
objektwerks <objektwerks@runbox.com>
2021-09-06 10:17:30 -0400

    enhanced Person

--- src/main/scala/objektwerks/Person.scala
+++ src/main/scala/objektwerks/Person.scala
@@ -1,6 +1,8 @@
 package objektwerks
 
-sealed trait Person extends Product with Serializable
+sealed trait Person extends Product with Serializable {
+  def name: String
+}
 final case class Employee(id: Int, name: String) extends Person
 object Employees {
   def newEmployee: Employee = Employee(1, "James Bond")
