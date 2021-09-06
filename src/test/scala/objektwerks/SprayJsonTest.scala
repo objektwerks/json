751ded922a50f59916f1bf994e2f4cbb87c14630
objektwerks <objektwerks@runbox.com>
2021-09-06 09:29:41 -0400

    refactored spray json test

--- src/test/scala/objektwerks/SprayJsonTest.scala
+++ src/test/scala/objektwerks/SprayJsonTest.scala
@@ -10,18 +10,16 @@ class SprayJsonTest extends AnyFunSuite with Matchers {
 
     val employee = Employee(1, "James Bond")
     val employeeJson = employee.toJson
+    employee shouldBe employeeJson.convertTo[Employee]
 
     println(s"spray json employee: $employee")
     println(s"spray json employee json: $employeeJson")
 
-    employee shouldBe employeeJson.convertTo[Employee]
-
     val person = employee.asInstanceOf[Person]
     val personJson = SprayJsonCodecs.write(person)
+    person shouldBe SprayJsonCodecs.read(personJson)
 
     println(s"spray json person: $person")
     println(s"spray json person json: $personJson")
-
-    person shouldBe SprayJsonCodecs.read(personJson)
   }
 }
