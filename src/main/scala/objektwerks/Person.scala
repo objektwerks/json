ecc3eef141c262bc0b4e7d792c68d87f657a9e38
objektwerks <objektwerks@runbox.com>
2021-09-06 09:55:40 -0400

    added employee object with factory method

--- src/main/scala/objektwerks/Person.scala
+++ src/main/scala/objektwerks/Person.scala
@@ -2,3 +2,6 @@ package objektwerks
 
 sealed trait Person extends Product with Serializable
 final case class Employee(id: Int, name: String) extends Person
+object Employee {
+  def newInstance: Employee = Employee(1, "James Bond")
+}
