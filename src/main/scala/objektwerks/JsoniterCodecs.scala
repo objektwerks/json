d382d2f461b4a06b5f32b8c30782cc2600ed7b3d
objektwerks <objektwerks@runbox.com>
2021-09-06 08:02:06 -0400

    refactored JsoniterCodecs

--- src/main/scala/objektwerks/JsoniterCodecs.scala
+++ src/main/scala/objektwerks/JsoniterCodecs.scala
@@ -1,9 +1,9 @@
 package objektwerks
 
-import com.github.plokhotnyuk.jsoniter_scala.core._
-import com.github.plokhotnyuk.jsoniter_scala.macros._
-
 object JsoniterCodecs {
+  import com.github.plokhotnyuk.jsoniter_scala.core._
+  import com.github.plokhotnyuk.jsoniter_scala.macros._
+
   implicit val personCodec: JsonValueCodec[Person] = JsonCodecMaker.make
   implicit val employeeCodec: JsonValueCodec[Employee] = JsonCodecMaker.make
 }
