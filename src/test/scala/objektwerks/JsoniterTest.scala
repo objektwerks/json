38e7ae6f8255ec3e47edc7557d8b17271089f466
objektwerks <objektwerks@runbox.com>
2021-09-06 08:06:48 -0400

    refactored JsoniterTest

--- src/test/scala/objektwerks/JsoniterTest.scala
+++ src/test/scala/objektwerks/JsoniterTest.scala
@@ -8,7 +8,7 @@ class JsoniterTest extends AnyFunSuite with Matchers {
     import JsoniterCodecs._
     import com.github.plokhotnyuk.jsoniter_scala.core._
 
-    val employee = Employee(id = 1, name = "auric goldfinger")
+    val employee = Employee(1, "auric goldfinger")
     val employeeJson = writeToString[Employee](employee)
     employee shouldBe readFromString[Employee](employeeJson)
 
