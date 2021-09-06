4c2990ba31c6320ba708692ed3db7e5852fa7801
objektwerks <objektwerks@runbox.com>
2021-09-06 08:10:33 -0400

    refactored borer test

--- src/test/scala/objektwerks/BorerTest.scala
+++ src/test/scala/objektwerks/BorerTest.scala
@@ -1,38 +1,28 @@
 package objektwerks
 
-import io.bullet.borer.Json
-import io.bullet.borer.derivation.ArrayBasedCodecs._
-
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
-sealed trait Canine extends Product with Serializable
-final case class Dog(breed: String, name: String) extends Canine
-
-object Canine {
-  implicit val dogCodec = deriveCodec[Dog]
-  implicit val canineCodec = deriveAllCodecs[Canine]
-}
-
 class BorerTest extends AnyFunSuite with Matchers {
   test("borer") {
-    import Canine._
+    import BorerCodecs._
+    import io.bullet.borer.Json
 
-    val dog = Dog("bulldog", "spike")
-    val dogJson = Json.encode(dog).toUtf8String
+    val employee = Employee(1, "spike")
+    val employeeJson = Json.encode(employee).toUtf8String
 
-    println(s"borer dog: $dog")
-    println(s"borer dog as json: $dogJson")
+    println(s"borer employee: $employee")
+    println(s"borer employee json: $employeeJson")
 
-    dog shouldBe Json.decode(dogJson.getBytes("UTF8")).to[Dog].value
+    employee shouldBe Json.decode(employeeJson.getBytes("UTF8")).to[Employee].value
 
-    val canine = dog.asInstanceOf[Canine]
-    canine.isInstanceOf[Canine] shouldBe true
-    val canineJson = Json.encode(canine).toUtf8String
+    val person = employee.asInstanceOf[Person]
+    person.isInstanceOf[Person] shouldBe true
+    val canineJson = Json.encode(person).toUtf8String
 
-    println(s"borer canine: $canine")
-    println(s"borer canine as json: $canineJson")
+    println(s"borer person: $person")
+    println(s"borer person json: $canineJson")
 
-    canine shouldBe Json.decode(canineJson.getBytes("UTF8")).to[Canine].value
+    person shouldBe Json.decode(canineJson.getBytes("UTF8")).to[Person].value
   }
 }
