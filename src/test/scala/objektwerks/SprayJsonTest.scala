e825663c3d3f704e6337d6aabf0ac0f865359c6e
objektwerks <objektwerks@runbox.com>
2021-09-06 08:48:29 -0400

    refactored spray json test

--- src/test/scala/objektwerks/SprayJsonTest.scala
+++ src/test/scala/objektwerks/SprayJsonTest.scala
@@ -3,45 +3,26 @@ package objektwerks
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
-import spray.json.{DefaultJsonProtocol, JsValue, enrichAny}
-
-sealed trait Color extends Product with Serializable {
-  val name: String
-  val code: String
-}
-
-final case class Navy(name: String, code: String) extends Color
-
-object ColorProtocol extends DefaultJsonProtocol {
-  implicit val navyFormat = jsonFormat2(Navy)
-
-  def write(color: Color): JsValue = color match {
-    case navy: Navy => navy.toJson
-  }
-
-  def read(json: JsValue): Color = json.convertTo[Navy]
-}
-
 class SprayJsonTest extends AnyFunSuite with Matchers {
   test("spray json") {
-    import ColorProtocol._
+    import ColorCodecs._
     import spray.json._
 
     val navy = Navy("navy", "#000080")
     val navyJson = navy.toJson
 
     println(s"spray json navy: $navy")
-    println(s"spray json navy as json: $navyJson")
+    println(s"spray json navy json: $navyJson")
 
     navy shouldBe navyJson.convertTo[Navy]
 
     val color = navy.asInstanceOf[Color]
     color.isInstanceOf[Color] shouldBe true
-    val colorJson = ColorProtocol.write(color)
+    val colorJson = ColorCodecs.write(color)
 
     println(s"spray json color: $color")
-    println(s"spray json color as json: $colorJson")
+    println(s"spray json color json: $colorJson")
 
-    color shouldBe ColorProtocol.read(colorJson)
+    color shouldBe ColorCodecs.read(colorJson)
   }
 }
