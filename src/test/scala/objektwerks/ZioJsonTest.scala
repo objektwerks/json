c54c70a097a75c6bebef2b8c4b0d4585ff114310
objektwerks <objektwerks@runbox.com>
2021-09-06 08:40:01 -0400

    refactored zio json test

--- src/test/scala/objektwerks/ZioJsonTest.scala
+++ src/test/scala/objektwerks/ZioJsonTest.scala
@@ -3,37 +3,26 @@ package objektwerks
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
-import zio.json._
-
-sealed trait Car extends Product with Serializable
-final case class Porsche(model: String, year: Int) extends Car
-
-object Car {
-  implicit val carDecoder: JsonDecoder[Car] = DeriveJsonDecoder.gen[Car]
-  implicit val carEncoder: JsonEncoder[Car] = DeriveJsonEncoder.gen[Car]
-  implicit val porscheDecoder: JsonDecoder[Porsche] = DeriveJsonDecoder.gen[Porsche]
-  implicit val porscheEncoder: JsonEncoder[Porsche] = DeriveJsonEncoder.gen[Porsche]
-}
-
 class ZioJsonTest extends AnyFunSuite with Matchers {
   test("zio") {
-    import Car._
+    import ZioJsonCodecs._
+    import zio.json._
 
-    val porsche = Porsche("911", 1996)
-    val porscheJson = porsche.toJson
+    val employee = Employee(1, "James Bond")
+    val employeeJson = employee.toJson
 
-    println(s"zio make: $porsche")
-    println(s"zio make as json: $porscheJson")
+    println(s"zio employee: $employee")
+    println(s"zio employee json: $employeeJson")
 
-    porsche shouldBe porscheJson.fromJson[Porsche].getOrElse( Porsche("", 0) )
+    employee shouldBe employeeJson.fromJson[Employee].getOrElse( Employee(0, "") )
 
-    val car = porsche.asInstanceOf[Car]
-    car.isInstanceOf[Car] shouldBe true
-    val carJson = car.toJson
+    val person = employee.asInstanceOf[Person]
+    person.isInstanceOf[Person] shouldBe true
+    val personJson = person.toJson
 
-    println(s"zio car: $car")
-    println(s"zio car as json: $carJson")
+    println(s"zio person: $person")
+    println(s"zio person json: $personJson")
 
-    car shouldBe carJson.fromJson[Car].getOrElse( Porsche("", 0) )
+    person shouldBe personJson.fromJson[Person].getOrElse( Employee(0, "") )
   }
 }
