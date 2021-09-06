f53aeb205c2e785d24ebc62416123b8f02320188
objektwerks <objektwerks@runbox.com>
2021-09-06 09:06:24 -0400

    removed color

--- src/main/scala/objektwerks/Color.scala
+++
@@ -1,8 +0,0 @@
-package objektwerks
-
-sealed trait Color extends Product with Serializable {
-  val name: String
-  val code: String
-}
-
-final case class Navy(name: String, code: String) extends Color
