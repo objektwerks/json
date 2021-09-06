8bed966593f81ad8625cb0bc95880050cd06a5b6
objektwerks <objektwerks@runbox.com>
2021-09-06 08:43:07 -0400

    refactored play json test

--- src/test/scala/objektwerks/PlayJsonTest.scala
+++ src/test/scala/objektwerks/PlayJsonTest.scala
@@ -3,35 +3,26 @@ package objektwerks
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
-sealed trait Identifier extends Product with Serializable
-final case class Name(first: String, last: String) extends Identifier
-object Name {
-  import play.api.libs.json.Json
-
-  implicit val nameFormat = Json.format[Name]
-  implicit val identifierFormat = Json.format[Identifier]
-}
-
 class PlayJsonTest extends AnyFunSuite with Matchers {
   test("play json") {
+    import PlayJsonCodecs._
     import play.api.libs.json.Json
-    import Name._
 
-    val name = Name("james", "bond")
-    val nameJson = Json.toJson(name).toString
+    val employee = Employee(1, "James Bond")
+    val employeeJson = Json.toJson(employee).toString
 
-    println(s"play json name: $name")
-    println(s"play json name as json: $nameJson")
+    println(s"play json employee: $employee")
+    println(s"play json employee json: $employeeJson")
 
-    name shouldBe Json.parse(nameJson).as[Name]
+    employee shouldBe Json.parse(employeeJson).as[Employee]
 
-    val identifier = name.asInstanceOf[Identifier]
-    identifier.isInstanceOf[Identifier] shouldBe true
-    val identifierJson = Json.toJson(identifier).toString
+    val person = employee.asInstanceOf[Person]
+    person.isInstanceOf[Person] shouldBe true
+    val identifierJson = Json.toJson(person).toString
 
-    println(s"play json identifier: $identifier")
-    println(s"play json identifier as json: $identifierJson")
+    println(s"play json person: $person")
+    println(s"play json person json: $identifierJson")
 
-    identifier shouldBe Json.parse(identifierJson).as[Identifier]
+    person shouldBe Json.parse(identifierJson).as[Person]
   }
 }
