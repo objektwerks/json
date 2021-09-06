2ae32bde046117364918d5e73c720f272bf943de
objektwerks <objektwerks@runbox.com>
2021-09-06 08:00:09 -0400

    refactored JsoniterTest

--- src/test/scala/objektwerks/JsoniterTest.scala
+++ src/test/scala/objektwerks/JsoniterTest.scala
@@ -3,20 +3,9 @@ package objektwerks
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
-sealed trait Person extends Product with Serializable
-final case class Employee(id: Int, name: String) extends Person
-
-object Codecs {
-  import com.github.plokhotnyuk.jsoniter_scala.core._
-  import com.github.plokhotnyuk.jsoniter_scala.macros._
-
-  implicit val personCodec: JsonValueCodec[Person] = JsonCodecMaker.make
-  implicit val employeeCodec: JsonValueCodec[Employee] = JsonCodecMaker.make
-}
-
 class JsoniterTest extends AnyFunSuite with Matchers {
   test("jsoniter") {
-    import Codecs._
+    import JsoniterCodecs._
     import com.github.plokhotnyuk.jsoniter_scala.core._
 
     val employee = Employee(id = 1, name = "auric goldfinger")
