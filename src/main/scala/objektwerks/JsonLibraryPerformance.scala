616c086f0787bd1b87e2df25a57fd26f8b1a76df
objektwerks <objektwerks@runbox.com>
2021-09-07 06:51:45 -0400

    added json library performance benchmark app

---
+++ src/main/scala/objektwerks/JsonLibraryPerformance.scala
@@ -0,0 +1,24 @@
+package objektwerks
+
+import java.util.concurrent.TimeUnit
+
+import org.openjdk.jmh.annotations._
+
+@State(Scope.Thread)
+@BenchmarkMode(Array(Mode.AverageTime))
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
+@Warmup(iterations = 3)
+@Measurement(iterations = 3)
+@Fork(1)
+class JsonLibraryPerformance {
+  @Benchmark
+  def borer(): Unit = {
+    import BorerCodecs._
+    import io.bullet.borer.Json
+
+    val employee = Employees.newEmployee
+    val employeeJson = Json.encode(employee).toUtf8String
+    assert( employee == Json.decode(employeeJson.getBytes("UTF8")).to[Employee].value )
+  }
+
+}
