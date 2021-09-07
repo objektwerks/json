616db683d0a038e44db06ba452a78851fa0188e4
objektwerks <objektwerks@runbox.com>
2021-09-07 07:56:50 -0400

    tuned performance benchmark

--- src/main/scala/objektwerks/Performance.scala
+++ src/main/scala/objektwerks/Performance.scala
@@ -11,8 +11,8 @@ import upickle.default._
 @State(Scope.Thread)
 @BenchmarkMode(Array(Mode.AverageTime))
 @OutputTimeUnit(TimeUnit.MICROSECONDS)
-@Warmup(iterations = 3)
-@Measurement(iterations = 3)
+@Warmup(iterations = 10)
+@Measurement(iterations = 10)
 @Fork(1)
 class Performance {
   @Benchmark
