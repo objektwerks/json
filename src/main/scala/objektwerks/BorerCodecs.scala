80259611316b786a473a078d2d9e01fb1c587278
objektwerks <objektwerks@runbox.com>
2022-05-18 07:22:23 -0400

    removed borer codecs

--- src/main/scala/objektwerks/BorerCodecs.scala
+++
@@ -1,8 +0,0 @@
-package objektwerks
-
-object BorerCodecs {
-  import io.bullet.borer.derivation.ArrayBasedCodecs._
-
-  implicit val employeeCodec = deriveCodec[Employee]
-  implicit val personCodec = deriveAllCodecs[Person]
-}
