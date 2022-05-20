b18fe950d052151611e556b643cf2b38caffaf0f
objektwerks <objektwerks@runbox.com>
2022-05-20 10:47:07 -0400

    edited upickle codecs

--- src/main/scala/objektwerks/UPickleCodecs.scala
+++ src/main/scala/objektwerks/UPickleCodecs.scala
@@ -4,4 +4,4 @@ object UPickleCodecs:
   import upickle.default.*
 
   given employeeRW: ReadWriter[Employee] = macroRW
-  given personRW: ReadWriter[Person] = ReadWriter.merge( macroRW[Employee] )
+  given personRW: ReadWriter[Person] = ReadWriter.merge( employeeRW )
