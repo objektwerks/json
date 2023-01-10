70062fdf653440e7df26b19544cc0975372ba7b5
objektwerks <objektwerks@runbox.com>
2023-01-10 06:34:08 -0500

    edited jsoniter codec

--- src/main/scala/objektwerks/JsoniterCodecs.scala
+++ src/main/scala/objektwerks/JsoniterCodecs.scala
@@ -4,13 +4,5 @@ object JsoniterCodecs:
   import com.github.plokhotnyuk.jsoniter_scala.core.*
   import com.github.plokhotnyuk.jsoniter_scala.macros.*
 
-  implicit val personCodec: JsonValueCodec[Person] =
-    JsonCodecMaker
-      .make[Person](
-        CodecMakerConfig.withDiscriminatorFieldName(None)
-      )
-  implicit val employeeCodec: JsonValueCodec[Employee] =
-    JsonCodecMaker
-      .make[Employee](
-        CodecMakerConfig.withDiscriminatorFieldName(None)
-      )
+  implicit val personCodec: JsonValueCodec[Person] = JsonCodecMaker.make[Person]( CodecMakerConfig.withDiscriminatorFieldName(None) )
+  implicit val employeeCodec: JsonValueCodec[Employee] = JsonCodecMaker.make[Employee]( CodecMakerConfig.withDiscriminatorFieldName(None) )
