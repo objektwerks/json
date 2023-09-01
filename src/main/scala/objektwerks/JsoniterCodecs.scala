28a1a942bc3eceb321c2fafd820c9fb88b48667f
objektwerks <objektwerks@runbox.com>
2023-09-01 15:59:39 -0400

    edited jsoniter codecs

--- src/main/scala/objektwerks/JsoniterCodecs.scala
+++ src/main/scala/objektwerks/JsoniterCodecs.scala
@@ -4,5 +4,5 @@ object JsoniterCodecs:
   import com.github.plokhotnyuk.jsoniter_scala.core.*
   import com.github.plokhotnyuk.jsoniter_scala.macros.*
 
-  given JsonValueCodec[Person] = JsonCodecMaker.make[Person]( CodecMakerConfig.withDiscriminatorFieldName(None) )
+  given JsonValueCodec[Person] = JsonCodecMaker.make[Person](CodecMakerConfig.withDiscriminatorFieldName(None))
   given JsonValueCodec[Employee] = JsonCodecMaker.make[Employee]
