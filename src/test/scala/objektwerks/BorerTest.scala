92726fe5e8a31cc76d55d7d138140d8dfe237a49
objektwerks <objektwerks@runbox.com>
2023-11-26 09:58:02 -0500

    added borer test

---
+++ src/test/scala/objektwerks/BorerTest.scala
@@ -0,0 +1,8 @@
+package objektwerks
+
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
+
+class BorerTest extends AnyFunSuite with Matchers:
+  test("borer"):
+    
