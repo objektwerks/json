ce4f63b9014b4bdef736221ef81bcdc5bef6e5dc
objektwerks <objektwerks@runbox.com>
2023-11-26 10:00:21 -0500

    enhanced borer test

--- src/main/scala/objektwerks/BorerCodecs.scala
+++ src/main/scala/objektwerks/BorerCodecs.scala
@@ -3,4 +3,5 @@ package objektwerks
 import io.bullet.borer.Codec
 import io.bullet.borer.derivation.MapBasedCodecs.*
 
+
 given Codec[Person] = deriveAllCodecs[Person]
--- src/test/scala/objektwerks/BorerTest.scala
+++ src/test/scala/objektwerks/BorerTest.scala
@@ -5,4 +5,5 @@ import org.scalatest.matchers.should.Matchers
 
 class BorerTest extends AnyFunSuite with Matchers:
   test("borer"):
-    
+    import io.bullet.borer.Json
+    import BorerCodecs.given
