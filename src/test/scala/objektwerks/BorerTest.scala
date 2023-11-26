c0bc95f2dfef88a604c0861589422573ee43686d
objektwerks <objektwerks@runbox.com>
2023-11-26 10:43:59 -0500

    corrected borer test

--- src/test/scala/objektwerks/BorerTest.scala
+++ src/test/scala/objektwerks/BorerTest.scala
@@ -5,20 +5,20 @@ import org.scalatest.matchers.should.Matchers
 
 class BorerTest extends AnyFunSuite with Matchers:
   test("borer"):
-    import io.bullet.borer.{Codec, Json}
+    import io.bullet.borer.Json
     import io.bullet.borer.derivation.MapBasedCodecs.*
     import BorerCodecs.given
 
     val employee = Employees.newEmployee
-    val employeeJson = Json.encode[Employee](employee).toUtf8String
-    employee shouldBe Json.decode[Employee](employeeJson)
+    val employeeJson = Json.encode(employee).toUtf8String
+    employee shouldBe Json.decode(employeeJson.getBytes("UTF8")).to[Employee].value
 
     println(s"borer employee: $employee")
     println(s"borer employee json: $employeeJson")
     
     val person = employee.asInstanceOf[Person]
-    val personJson = Json.encode[Person](person).toUtf8String
-    person shouldBe Json.decode[Person](personJson)
+    val personJson = Json.encode(person).toUtf8String
+    person shouldBe Json.decode(personJson.getBytes("UTF8")).to[Person].value
 
     println(s"borer person: $person")
     println(s"borer person json: $personJson")
